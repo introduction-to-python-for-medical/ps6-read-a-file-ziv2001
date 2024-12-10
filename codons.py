5bf457176d75d92e5412bdd447e3ca8fd9775ff5
ziv2001 <ziv222444666@gmail.com>
2024-12-10 17:29:42 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,11 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    codon_to_amino = {}
+    for row in rows:
+        split_row = row.strip().split('\t')
+    codon_to_amino[split_row[0]]=split_row[2]
+    return codon_to_amino
 
 
